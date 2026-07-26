bb1eea4ca7873d70447412c5dd71575d683f297b
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2026-07-26 16:29:04 +0900

    using 추가, 불변 IReadonlyList규칙 추가, 줄바꿈 변경

--- Assets/1_Scripts/1_Domain/MoveDomain.cs
+++ Assets/1_Scripts/1_Domain/MoveDomain.cs
@@ -1,26 +1,19 @@
+using System.Collections.Generic;
+using System.Linq;
+
 public static class PuzzleDomain
 {
-    public static Option<ChessSquare> ValidateSquare(ChessSquare targetSquare, ImmutableList<ChessSquare> allowedSquares)
+    public static Option<ChessSquare> ValidateSquare(ChessSquare targetSquare, IReadOnlyList<ChessSquare> allowedSquares)
     {
         bool _isAllowed = allowedSquares.Contains(targetSquare);
-
-        return _isAllowed
-            ? Option<ChessSquare>.Some(targetSquare)
-            : Option<ChessSquare>.None;
+        return _isAllowed ? Option<ChessSquare>.Some(targetSquare) : Option<ChessSquare>.None;
     }
 
     public static Option<PiecesType> GetPiece(BoardState boardState, ChessSquare targetSquare)
     {
         PiecesType? _foundPiece = boardState.GetPieceAt(targetSquare);
-
-        return _foundPiece.HasValue
-            ? Option<PiecesType>.Some(_foundPiece.Value)
-            : Option<PiecesType>.None;
+        return _foundPiece.HasValue ? Option<PiecesType>.Some(_foundPiece.Value) : Option<PiecesType>.None;
     }
 
-    public static PuzzlePlayState CreatePuzzlePlayState(PiecesType selectedPiece)
-    {
-        PuzzlePlayState _newState = new PuzzlePlayState(PuzzleStatusType.Select, selectedPiece);
-        return _newState;
-    }
+    public static PuzzlePlayState CreatePuzzlePlayState(PiecesType selectedPiece) => new PuzzlePlayState(PuzzleStatusType.Select, selectedPiece);
 }
