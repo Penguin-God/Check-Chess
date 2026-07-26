478a6915cd4bfb9f57fae2fb2f038d32de1ffbbb
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2026-07-26 16:33:29 +0900

    BoardState 구현

--- Assets/1_Scripts/1_Domain/Datas.cs
+++ Assets/1_Scripts/1_Domain/Datas.cs
@@ -20,7 +20,29 @@ public record ChessSquare(int X, int Y);
 
 public record PuzzlePlayState(PuzzleStatusType Status, PiecesType Piece);
 
-public class BoardState
+public record BoardState
 {
-    
+    private readonly PiecesType?[,] grid;
+
+    public int Width { get; }
+    public int Height { get; }
+
+    public BoardState(PiecesType?[,] initialGrid)
+    {
+        grid = initialGrid;
+        Width = grid.GetLength(0);
+        Height = grid.GetLength(1);
+    }
+
+    public PiecesType? GetPieceAt(ChessSquare square)
+    {
+        bool _isOutOfBounds = square.X < 0 || square.X >= Width || square.Y < 0 || square.Y >= Height;
+
+        if (_isOutOfBounds)
+        {
+            return null;
+        }
+
+        return grid[square.X, square.Y];
+    }
 }
