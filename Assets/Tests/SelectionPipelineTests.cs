8f4bf056b828120c9c2f5ed227f566ad582419e0
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2026-07-26 16:46:31 +0900

    칸 검증 테스트 작성

--- Assets/Tests/SelectionPipelineTests.cs
+++ Assets/Tests/SelectionPipelineTests.cs
@@ -3,5 +3,14 @@ using System.Collections.Generic;
 
 public class SelectionPipelineTests
 {
+    [Test]
+    [TestCase()]
+    public void 사용_가능한_칸인지_검증(int x, int y)
+    {
+        var allowedSquares = new List<ChessSquare>() { DataFactory.CreateSquare(1, 1) };
 
+        var result = PuzzleDomain.ValidateSquare(DataFactory.CreateSquare(x, y), allowedSquares);
+
+        // Assert.AreEqual(result, );
+    }
 }
