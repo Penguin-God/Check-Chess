73a253a61b839c53c07b49dc716db3b405278942
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2026-07-30 21:07:15 +0900

    클래스 이름 변경 Calculator -> Helper

--- Assets/1_Scripts/UI/BoardUICalculator.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public static class BoardUICalculator
-{
-    public static Color GetCheckerboardColor(int x, int y, Color lightColor, Color darkColor) => (x + y) % 2 == 0 ? darkColor : lightColor;
-}
