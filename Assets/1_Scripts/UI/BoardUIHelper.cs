085225dde10d6a2584cd9cab5dbfdf2111c48603
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2026-07-30 21:07:41 +0900

    보드 그리는 루프 고차함수 추가

---
+++ Assets/1_Scripts/UI/BoardUIHelper.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+public static class BoardUIHelper
+{
+    public static Color GetCheckerboardColor(BoardCoord boardCoord, Color lightColor, Color darkColor) => (x + y) % 2 == 0 ? darkColor : lightColor;
+
+    public static void DrawBoard(Action<BoardCoord> onDrawSquare)
+    {
+        const int BOARD_SIZE = 8;
+        for (int y = 0; y < BOARD_SIZE; y++)
+        {
+            for (int x = 0; x < BOARD_SIZE; x++)
+            {
+                onDrawSquare(new BoardCoord(x, y));
+            }
+        }
+    }
+}
