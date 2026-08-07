787079bb6beb694ca970b4e52a8bc05869956e20
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2026-08-07 22:10:17 +0900

    이미지가 칸의 상태를 나타낸다는 것을 의미하도록 이름 변경

--- Assets/1_Scripts/UI/UI_Square.cs
+++ Assets/1_Scripts/UI/UI_Square.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
 
-public record SquareModel(Color BgColor, Sprite Sprite);
+public record SquareModel(Color BgColor, Sprite statusIcon);
 
 public class UI_Square : MonoBehaviour
 {
     [Header("UI Components")]
     public Button button;
     public Image background;
-    public Image pieceImage;
+    public Image statusIcon;
 
     // 초기화 및 클릭 이벤트 연결을 도와주는 헬퍼 함수
     public void Init(UnityAction onClickAction)
@@ -21,14 +21,14 @@ public class UI_Square : MonoBehaviour
     public void UpdateVisuals(Color bgColor, Sprite sprite, Color spriteColor)
     {
         background.color = bgColor;
-        pieceImage.sprite = sprite;
-        pieceImage.color = spriteColor;
+        statusIcon.sprite = sprite;
+        statusIcon.color = spriteColor;
     }
 
     public void UpdateVisuals(SquareModel squareModel)
     {
         background.color = squareModel.BgColor;
-        pieceImage.sprite = squareModel.Sprite;
-        pieceImage.color = squareModel.Sprite == null ? Color.clear : Color.white;
+        statusIcon.sprite = squareModel.statusIcon;
+        statusIcon.color = squareModel.statusIcon == null ? Color.clear : Color.white;
     }
 }
