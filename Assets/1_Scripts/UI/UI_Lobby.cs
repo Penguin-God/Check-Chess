9c588ae9fd00ab2f77ad2068115903ce83a8d4bd
Penguin-God <60744738+Penguin-God@users.noreply.github.com>
2026-08-08 17:04:37 +0900

    여러 bool을 enum 상태로 관리

--- Assets/1_Scripts/UI/UI_Lobby.cs
+++ Assets/1_Scripts/UI/UI_Lobby.cs
@@ -1,13 +1,20 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
+public enum StageState
+{
+    Locked,
+    Playable,
+    Unreached
+}
 
 public class UI_Lobby : MonoBehaviour
 {
     [Header("Board UI References")]
     public Transform boardPanel;
     public GameObject squarePrefab;
-    public GameObject lockIconPrefab;
     public RectTransform pawnMarker;
     public Sprite pawnIcon;
     public Sprite lockSprite;
@@ -17,7 +24,6 @@ public class UI_Lobby : MonoBehaviour
     public Color darkSquareColor = new Color(0.4f, 0.6f, 0.4f);
 
     BoardThemeSO boardTheme;
-
     Board<Button> boardButtons = new();
 
     void Start()
@@ -30,19 +36,6 @@ public class UI_Lobby : MonoBehaviour
     {
         GameObject obj = Instantiate(squarePrefab, boardPanel);
         boardButtons = boardButtons.Change(coord, obj.GetComponent<Button>());
-
-        // 자물쇠 아이콘 세팅 (동적으로 프리팹 생성 후 비활성화)
-        Transform lockTransform = obj.transform.Find("LockIcon");
-        if (lockTransform == null && lockIconPrefab != null)
-        {
-            GameObject lockObj = Instantiate(lockIconPrefab, obj.transform);
-            lockObj.name = "LockIcon";
-            lockObj.SetActive(false);
-        }
-        else if (lockTransform != null)
-        {
-            lockTransform.gameObject.SetActive(false);
-        }
     }
 
     void UpdateLobbyUI()
@@ -56,17 +49,13 @@ public class UI_Lobby : MonoBehaviour
             int absoluteLevel = (coord.X * BoardIterator.BOARD_SIZE) + coord.Y;
             Button btn = boardButtons[coord];
 
-            // TODO : 이거 enum상태로 만들기
-            bool isReached = StageLockLogic.IsReached(absoluteLevel, maxCleared);
-            bool isCurrentlyLocked = StageLockLogic.IsCurrentlyLocked(absoluteLevel, lockedSet, unlockedSet);
-            bool canPlay = isReached && !isCurrentlyLocked;
-
-            // 2. 부수 효과: 시각적 상태 적용
-            btn.interactable = canPlay || isCurrentlyLocked; // 플레이 가능하거나, 자물쇠를 풀 수 있거나
-            ApplySquareVisuals(btn, coord, canPlay, isCurrentlyLocked);
+            // 1. 순수 함수로 현재 칸의 명확한 '상태(State)' 하나를 도출
+            StageState currentState = EvaluateStageState(absoluteLevel, maxCleared, lockedSet, unlockedSet);
+            btn.interactable = currentState != StageState.Unreached; // Unreached가 아닐 때만 터치 가능
+            ApplySquareVisuals(btn, coord, currentState);
 
             // 3. 버튼 동작 연결
-            BindButtonAction(btn, absoluteLevel, canPlay, isCurrentlyLocked);
+            BindButtonAction(btn, absoluteLevel, currentState);
 
             // 4. 플레이어 위치(폰) 갱신
             if (absoluteLevel == maxCleared)
@@ -76,48 +65,55 @@ public class UI_Lobby : MonoBehaviour
         });
     }
 
+    StageState EvaluateStageState(int absoluteLevel, int maxCleared, HashSet<int> lockedSet, HashSet<int> unlockedSet)
+    {
+        if (StageLockLogic.IsCurrentlyLocked(absoluteLevel, lockedSet, unlockedSet)) return StageState.Locked;
+        if (StageLockLogic.IsReached(absoluteLevel, maxCleared)) return StageState.Playable;
+        return StageState.Unreached;
+    }
+
     void RenderState(BoardCoord coord)
     {
         var modelBoard = BoardModelMapper.CreateEmptyModel(boardTheme.lightSquareColor, boardTheme.darkSquareColor);
-        modelBoard = modelBoard.Change(coord, modelBoard[coord] with { StatusIcon = pawnIcon } );
+        modelBoard = modelBoard.Change(coord, modelBoard[coord] with { StatusIcon = pawnIcon });
     }
 
     void Draw(BoardCoord coord, SquareModel model)
     {
-
     }
 
-    void ApplySquareVisuals(Button btn, BoardCoord coord, bool canPlay, bool isCurrentlyLocked)
+
+    // --- [ 부수 효과 (Side Effects) ] ---
+
+    void ApplySquareVisuals(Button btn, BoardCoord coord, StageState state)
     {
         UI_Square squareUI = btn.GetComponent<UI_Square>();
-
         Color baseColor = BoardIterator.GetCheckerboardColor(coord, lightSquareColor, darkSquareColor);
-        Color finalBgColor = canPlay ? baseColor : Color.Lerp(baseColor, Color.gray, 0.7f);
-
-        Sprite currentIcon = null;
-        if (isCurrentlyLocked) // 잠겨있다면 자물쇠 스프라이트 지정
-            currentIcon = lockSprite;
+        Color finalBgColor = state == StageState.Playable ? baseColor : Color.Lerp(baseColor, Color.gray, 0.7f);
+        Sprite currentIcon = state == StageState.Locked ? lockSprite : null;
 
         SquareModel squareModel = new SquareModel(finalBgColor, currentIcon);
         squareUI.UpdateVisuals(squareModel);
     }
 
-    void BindButtonAction(Button btn, int absoluteLevel, bool canPlay, bool isCurrentlyLocked)
+    void BindButtonAction(Button btn, int absoluteLevel, StageState state)
     {
         btn.onClick.RemoveAllListeners();
 
-        if (isCurrentlyLocked) btn.onClick.AddListener(() => WatchAdToUnlock(absoluteLevel));
-        else if (canPlay) btn.onClick.AddListener(() => OnStageSelected(absoluteLevel));
+        switch (state)
+        {
+            case StageState.Locked: btn.onClick.AddListener(() => WatchAdToUnlock(absoluteLevel)); break;
+            case StageState.Playable: btn.onClick.AddListener(() => OnStageSelected(absoluteLevel)); break;
+            case StageState.Unreached: break;
+        }
     }
 
     void WatchAdToUnlock(int absoluteLevel)
     {
         Debug.Log($"레벨 {absoluteLevel} 자물쇠 해금을 위해 광고를 봅니다.");
 
-        // 광고 매니저 호출 및 성공 시 실행될 콜백 전달
         LevelPlayAdManager.Instance.ShowRewardedAd(() =>
         {
-            // 성공 시 자물쇠 해제 및 UI 즉시 갱신
             StageLockManager.Instance.UnlockLevel(absoluteLevel);
             UpdateLobbyUI();
         });
